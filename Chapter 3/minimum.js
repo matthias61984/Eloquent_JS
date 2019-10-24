29b7787047c0b08e5db9a3407b2ea446d475dd47
Corey Walsh <matthias61984@hotmail.com>
2019-10-24 13:01:58 -0700

    Increase minimum.js test cases

--- Chapter 3/minimum.js
+++ Chapter 3/minimum.js
@@ -7,6 +7,13 @@ function min(x, y) {
 };
 
 console.log(min(0, 10));
+console.log(min(10, 0));
+console.log(min(5, 10));
 console.log(min(0, 0));
-console.log(min(0, -10));
+console.log(min(10, 10));
+console.log('-----');
+console.log(min(10, -10));
 console.log(min(-10, 10));
+console.log(min(-10, -5));
+console.log(min(-5, -10));
+console.log(min(-10, -10));
