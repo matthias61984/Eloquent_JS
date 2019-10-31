d6da33a5f9f36d76f1f909e8182e3011e88bd79b
CoreyWalsh <matthias61984@hotmail.com>
2019-10-31 15:09:43 -0700

    Solved reversingAnArrayInPlace

--- Chapter 4/reversingAnArray.js
+++ Chapter 4/reversingAnArray.js
@@ -9,16 +9,22 @@ function reverseArray(arr) {
 }
 
 function reverseArrayInPlace(arr) {
-    var counter = arr.length;
 
-    while (counter > 0) {
-        arr.push(arr.pop());
-        counter--;
-        console.log(arr);
+    for (var i = 0; i < Math.floor(arr.length / 2) ; i++) {
+        var sub = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = sub;
     }
-    
+
     return arr;
 }
 
-//console.log(reverseArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(reverseArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(reverseArray([10, 11, 12, 13, 14, 15]));
+console.log(reverseArray(['Apples', 'Oranges', 'Bananas', true, false, 1254, 'More Bananas']));
+
+
 console.log(reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(reverseArrayInPlace([10, 11, 12, 13, 14, 15]));
+console.log(reverseArrayInPlace(['Apples', 'Oranges', 'Bananas', true, false, 1254, 'More Bananas']));
+
