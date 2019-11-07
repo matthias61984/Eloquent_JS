6a57c5ae1cd4db51c91c82cfa408b63970f118d5
CoreyWalsh <matthias61984@hotmail.com>
2019-11-06 18:57:35 -0800

    Solved and reviewed deepEqual part 2

--- Chapter 4/deepEqual.js
+++ Chapter 4/deepEqual.js
@@ -1,15 +1,21 @@
 var obj = { here: { is: "an" }, object: 2 };
 
 function deepEqual(one, two) {
+    if (one === two) return true; 
     
-    if (typeof one == typeof two) {
+    if (one == null || two == null || typeof one != 'object' || typeof two != 'object') return false;
+
+    var propertiesInOne = 0;
+    var propertiesInTwo = 0;
+
+    for (var prop in one) propertiesInOne += 1;
+
+    for (var prop in two) {
+        propertiesInTwo += 1;
+        if (!(prop in one) || !deepEqual(one[prop], two[prop])) return false;
     }
-    
-    if (one === two) {
-        return true;
-    } else {
-        return false;
-    }
+
+    return propertiesInOne == propertiesInTwo;
 }
 
 console.log(deepEqual(obj, obj));
