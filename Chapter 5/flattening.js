1d51c588324c817bfa14c65c7f39ada5e511d901
CoreyWalsh <matthias61984@hotmail.com>
2020-01-15 18:58:51 -0800

    Solved Chapter 5 flattening

--- Chapter 5/flattening.js
+++ Chapter 5/flattening.js
@@ -1,15 +1,23 @@
 var arrays = [[1, 2, 3], [4, 5], [6]];
+var stringArrays = [["John", "Snow"], ["the"], ["King"], ["in", "the", "North"]];
 
-function flatten(arrays) {
-    var flattened = [];
+// function flatten(arrays) {
+//     var flattened = [];
     
-    for (var i = 0; i < arrays.length; i++) {
-        flattened.reduce(flatten(arrays[i]));
-        flattened.concat(arrays[i]);
-        console.log(arrays[i]);
-    }
+//     for (var i = 0; i < arrays.length; i++) {
+//         flattened = flattened.concat(arrays[i]);
+//     }
 
-    return flattened;
-}
+//     return flattened;
+// }
 
-console.log(flatten(arrays));
+console.log(arrays.reduce(function(flat, current) {
+    return flat.concat(current);
+}, []));
+
+console.log(stringArrays.reduce(function(flat, current) {
+    return flat.concat(current);
+}, []));
+
+// console.log(flatten(arrays));
+// console.log(flatten(stringArrays));
